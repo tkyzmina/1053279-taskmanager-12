10ced209c898c94068bc992561241793f279ce06
tkyzmina <tkyzmina@gmail.com>
2020-08-01 18:51:08 +0300

    разбивает на блоки

---
+++ src/view/new-task-button.js
@@ -0,0 +1,3 @@
+export const createNewTaskButton = () => {
+  return (`<button class="load-more" type="button">load more</button>`);
+};
---
+++ src/view/sort.js
@@ -0,0 +1,14 @@
+export const createSortTemplate = () => {
+  return (
+    `<section class="board container">
+  <div class="board__filter-list">
+    <a href="#" class="board__filter">SORT BY DEFAULT</a>
+    <a href="#" class="board__filter">SORT BY DATE up</a>
+    <a href="#" class="board__filter">SORT BY DATE down</a>
+  </div>
+
+  <div class="board__tasks">
+  </div>
+</section>`
+  );
+};
